5aeef49954014214ceea8be5cdc3132cd99bdf93
deepak patel <deepak20@navgurukul.org>
2021-09-16 17:38:44 +0530

    routes connected

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose')
+const authRoute = require('./routes/auth');
+const customerRoute = require('./routes/customer');
+const genresRoute = require('./routes/geners');
+const movieRoute = require('./routes/movies');
+const rentalsRoute = require('./routes/rentals');
+const returnsRoute = require('./routes/returns');
+const usersRoute = require('./routes/users');
 require('dotenv').config();
 const app = express();
 // require('./startup/routes')(app);
@@ -10,6 +17,15 @@ const app = express();
 mongoose.connect(process.env.DATABASE)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
+
+  app.use(express.json());
+  app.use('/api/users',usersRoute);
+  app.use('/api/returns',returnsRoute);
+  app.use('/api/rentals',rentalsRoute);
+  app.use('/api/movie',movieRoute);
+  app.use('/api/genre',genresRoute);
+  app.use('/api/customer',customerRoute);
+  app.use('/api/auth',authRoute)
 
 
 const port = process.env.PORT || 4000; 
